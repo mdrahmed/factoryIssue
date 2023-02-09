bbd953dbe706a7171e9bd45f6947a0cab4379a74
MdRaihanAhmed-coder <raihan.ahmed05.96@gmail.com>
2023-02-08 18:55:16 -0700

    same function were modified multiple times

--- pass/shortPass.cpp
+++ pass/shortPass.cpp
@@ -53,28 +53,48 @@ bool CPSTracker::runOnModule(Module &M) {
         FunctionType *printfType = FunctionType::get(intType, printfArgsTypes, true);
         auto printfFunc = M.getOrInsertFunction("printf", printfType);
 
+
+	std::vector<StringRef> functions;	
+	std::vector<StringRef> alp_funcs;
+
+	int i,alp;
+	i = 0;
+	alp = 0;
+
 	for (auto &F:M){
 		std::vector<Value*> arg_values; // used to store functions argument values
 		// Marking how many values a function has 
 		for(auto i = F.arg_begin();i!=F.arg_end();++i){
 			arg_values.push_back(i);
 		}
-	
+		const DataLayout &DL = M.getDataLayout();
 		
 		// I am instrumenting only these certain functions
-		if(F.getName().contains("message_arrived") || F.getName().contains("publish") || F.getName().contains("requestOrder") || F.getName().contains("startThread") || F.getName().contains("start_thread") || F.getName().contains("run") || F.getName().contains("fsmStep") || F.getName().contains("printState") || F.getName().contains("setTarget") || F.getName().contains("moveDeliveryInAndGrip") || F.getName().contains("moveNFC") ){
-			if(F.getName().contains("publish"))
-				errs()<<F.getName()<<"\n";	
-			if(F.getName() == "printf" || F.getName().startswith("llvm.dbg"))
-				continue;
-			
+		//if(F.getName().contains("message_arrived") || F.getName().contains("publish") || F.getName().contains("requestOrder") || F.getName().contains("startThread") || F.getName().contains("start_thread") || F.getName().contains("fsmStep") || F.getName().contains("printState") || F.getName().contains("setTarget") || F.getName().contains("moveDeliveryInAndGrip") || F.getName().contains("moveNFC") ){
+		if(F.getName().contains("publish")){
+			errs()<<F.getName()<<"\n";	
+			if(F.getName().contains("action_listener_publish")){
+				alp++;	
+				alp_funcs.push_back(F.getName());
+			}	
+			for (auto &f : functions){
+				errs()<<"No."<<i<<" f is: "<<f<<"\n";
+				i++;
+				if(f == F.getName()){
+					errs()<<"Function found: "<<f<<"\n\n";
+					continue;
+				}
+			}
+			functions.push_back(F.getName());
+			errs()<<"New functions inserted is: "<<F.getName()<<"\n";
+
 			// If a function is declared then it will not have basic blocks in them. So, if a function is not delcared then it will have basic block, which I need to insert printf
 			if(!F.isDeclaration()){
 				auto &BB = F.getEntryBlock();        
 				std::vector<std::string> arguments; // This vector will be used to store functions arguments name
 				BasicBlock::iterator IP = BB.getFirstInsertionPt();
 				IRBuilder<> builder(&(*IP));
-				
+				//errs()<<"Func is not declared: "<<F.getName()<<"\n";
 				{
 					
 					// Add string format specifier for each argument we will later print
@@ -86,20 +106,26 @@ bool CPSTracker::runOnModule(Module &M) {
 					std::vector<Value *> argsV({str});
 					// If I simply push the values then it works fine but I have to get values for arm-32 bit.
 					// That's why I am bitcasting the values to a 32-bit result and then pushing it. But only this part is causing the error. 
+					errs()<<"Inside values part: "<<F.getName()<<"\n";
 					for (auto &v : arg_values) {
 					        argsV.push_back(builder.CreateGlobalStringPtr(v->getName(), ""));
-					        const DataLayout &DL = M.getDataLayout();
+					        //const DataLayout &DL = M.getDataLayout();
 					        unsigned SourceBitWidth = DL.getTypeSizeInBits(v->getType());
 					        //unsigned SourceBitWidth = cast<IntegerType>(v->getType())->getBitWidth();;
 					        IntegerType *IntTy = builder.getIntNTy(SourceBitWidth);
 					        Value *IntResult = builder.CreateBitCast(v, IntTy);
 					        Value *Int32Result = builder.CreateSExtOrTrunc(IntResult, Type::getInt32Ty(context));
+						errs()<<"Iterating values:  "<<Int32Result<<"\n";
 					        argsV.push_back(Int32Result);
 					}
 					builder.CreateCall(printfFunc, argsV, "calltmp"); 
 				}
 			}
 		}
+	}
+	errs()<<"action_listener_publish is instrumented: "<<alp<<"\n";
+	for(auto &alpf: alp_funcs){
+		errs()<<alpf<<"\n";
 	}
         return true;
 
